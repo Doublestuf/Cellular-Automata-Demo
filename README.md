f42e6e2041c0345d98f3971691ec08d2884bfe4f
Doublestuf <153161997+Doublestuf@users.noreply.github.com>
2023-12-17 03:04:31 -0500

    improved algorithm to sort length-wise and width-wise, and other changes

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # Cellular-Automata-Demo
 A demo of the cellular automata system as used for world generation
 
-implemented in pygame (obviously not a fully correct implementation but you get the idea)
+a slightly imperfect implementation, made in pygame
 
 How it works:
     Randomly makes each tile either grass or water
-    Checks if the tiles to the left and right of a tile are grass. If the tile is water, it changes to grass
+    Checks if the tiles to the left and right of a tile are grass. If the middle tile is water, it changes to grass
+    Checks if the tiles to the left and right of a tile are water. If the middle tile is grass, it changes to water
+    Repeat 10x
     That's it
 
 Controls:
     ENTER to randomly fill the tiles
-    SPACE to check the tiles next to each tile
+    SPACE to iterate over the tiles 10 time
+    B to iterate over the tiles vertically
+    N to iterate over the tiles horizontally
+    M to iterate over the tiles horizontally, then vertically
 
-Hope you learned something i guess
+Hope this helps i guess
+Feel free to use in any project, educational, non-commercial, commercial, credit optional
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame as pg
 import random
 
+pg.font.init()
+
 window = pg.display.set_mode((1000, 1000))
 clock = pg.time.Clock()
 
 cell_rects = []
 cells_filled = {}
+
+width_cell_rects = []
 
 filled_cell_amount = 0
 empty_cell_amount = 0
@@ -22,6 +26,12 @@ def find_cells():
         for y in range(0, window.get_height(), 10):
             cell = pg.Rect(x, y, 10, 10)
             cell_rects.append(cell)
+            
+def find_cells_width():
+    for y in range(0, window.get_width(), 10):
+        for x in range(0, window.get_height(), 10):
+            cell = pg.Rect(x, y, 10, 10)
+            width_cell_rects.append(cell)
             
 def fill_random_cells():
     global filled_cell_amount, empty_cell_amount
@@ -56,8 +66,33 @@ def check_nearby_tiles():
         
         if left_filled and right_filled and not filled:
             cells_filled[location] = True
+        elif not left_filled and not right_filled and filled:
+            cells_filled[location] = False
+            
+def check_nearby_tiles_width():
+    for index, location in enumerate(cells_filled):
+        left_index = index-1
+        left_location = width_cell_rects[left_index].topleft
+        left_filled = cells_filled[left_location]
+        
+        right_index = index+1
+        try:
+            right_location = width_cell_rects[right_index].topleft
+        except:
+            continue
+        right_filled = cells_filled[right_location]
+        
+        filled = cells_filled[location]
+        
+        if left_filled and right_filled:
+            cells_filled[location] = True
+        if not left_filled and not right_filled:
+            cells_filled[location] = False
 
 running = True
+iterations = False
+
+find_cells_width()
 
 while running:
     window.fill((0, 0, 255))
@@ -65,7 +100,32 @@ while running:
     draw_grid()
     draw_filled_cells()
     
-    clock.tick(60)
+    if iterations == -1:
+        font = pg.font.Font(pg.font.get_default_font(), 50)
+        text = font.render("Cleaning up...", False, (255, 255, 255))
+        window.blit(text, (0, 0))
+        
+        check_nearby_tiles_width()
+        check_nearby_tiles_width()
+        check_nearby_tiles_width()
+        check_nearby_tiles()
+        check_nearby_tiles()
+        check_nearby_tiles()
+        
+        iterations = False
+
+    if iterations:
+        check_nearby_tiles_width()
+        check_nearby_tiles()
+        draw_filled_cells()
+        
+        font = pg.font.Font(pg.font.get_default_font(), 50)
+        text = font.render(f"Iterations Left: {iterations}", False, (255, 255, 255))
+        window.blit(text, (0, 0))
+        
+        iterations = iterations - 1 if iterations > 1 else -1
+    
+    clock.tick()
     pg.display.update()
     
     for event in pg.event.get():
@@ -82,5 +142,11 @@ while running:
                 find_cells()
                 fill_random_cells()
             elif event.key == pg.K_SPACE:
-                print("rawr")
+                iterations += 10
+            elif event.key == pg.K_b:
+                check_nearby_tiles()
+            elif event.key == pg.K_n:
+                check_nearby_tiles_width()
+            elif event.key == pg.K_m:
+                check_nearby_tiles_width()
                 check_nearby_tiles()
